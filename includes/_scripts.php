59632c4e46e798c09750350bbc2d72632c586419
Ziyad S. Zorub <zi.zorub@gmail.com>
2022-08-08 18:04:42 +0200

    Shorooq

--- includes/_scripts.php
+++ includes/_scripts.php
@@ -1,5 +1,6 @@
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 
+<script src="assets/dist/js/bootstrap.bundle.min.js"></script>
 <script src="assets/dist/js/vendor/popper.min.js"></script>
 <script src="assets/dist/js/bootstrap.min.js"></script>
 
--- test.php
+++ test.php
@@ -2,13 +2,7 @@
 <html dir="rtl">
 <head>
 	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1"><link href="https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Source+Serif+Pro:400,600&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="src/css/style.css">
-    <link rel="stylesheet" href="src/css/owl.carousel.min.css">
-    <!-- Style -->
-    <link rel="stylesheet" href="src/css/style.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title></title>
 	<style type="text/css">
 		* {
@@ -22,50 +16,13 @@
 	<!------------------------------>
 </head>
 <body class="p-0">
-	<p class="bg-danger text-center h1">
-		<a class="text-white" href="index.php">It's Just Test Site!</a><hr>
-	</p>
-	<div class="container">
-	<div class="owl-carousel owl-1">
-		<div class="media-29121 overlay" style="background-image: url('src/images/hero_1.jpg');">
-			<div class="container">
-				<div class="row justify-content-center align-items-end text-center">
-					<div class="col-md-7">
-						<h2 class="mb-5">Impedit Ipsam Quae Natus</h2>
-					</div>
-				</div>
-			</div>
-		</div> <!-- .item -->
-		<div class="media-29121 overlay" style="background-image: url('src/images/hero_2.jpg');">
-			<div class="container">
-				<div class="row justify-content-center align-items-end text-center">
-					<div class="col-md-7">
-						<h2 class="mb-5">Impedit Ipsam Quae Natus</h2>
-					</div>
-				</div>
-			</div>
-		</div> <!-- .item -->
-		<div class="media-29121 overlay" style="background-image: url('src/images/hero_3.jpg');">
-			<div class="container">
-				<div class="row justify-content-center align-items-end text-center">
-					<div class="col-md-7">
-						<h2 class="mb-5">Impedit Ipsam Quae Natus</h2>
-					</div>
-				</div>
-			</div>
-		</div> <!-- .item -->
-
-	</div>
-</div>
 	<!------- D e m o -------------->
 	<?php  
-		//require 'demo.html';
+		require 'demo.html';
 	?>
 	<!------------------------------>
 	<!------- Scripts -------------->
-	<script src="src/js/jquery-3.3.1.min.js"></script>
-    <script src="src/js/owl.carousel.min.js"></script>
-    <script src="src/js/main.js"></script>
+	
 	<?php
 		require 'includes/_scripts.php';
 	?>
